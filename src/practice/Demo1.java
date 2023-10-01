f77029b3e6409b5630af75e9b9ac66741e3b0798
ssharathchandraqa950 <106028160+ssharathchandraqa950@users.noreply.github.com>
2023-10-01 15:03:09 +0530

    Update Demo1.java

--- src/practice/Demo1.java
+++ src/practice/Demo1.java
@@ -6,5 +6,7 @@ public class Demo1 {
 		System.out.println("GitJenkinsPractice_Project");
 		System.out.println("Adding this line to check GitHub PollSCM");
 		System.out.println("Adding this line in slave1 branch");
+		System.out.println("Adding this line to check fetch command");
+		
 	}
 }
