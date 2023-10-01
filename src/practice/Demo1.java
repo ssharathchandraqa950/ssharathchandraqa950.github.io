6801bbdbbb3e57e4eae7874356ae908d2ac538f0
ssharathchandraqa950 <ssharathchandraqa950@gmail.com>
2023-10-01 14:21:25 +0530

    adding in slave1

--- src/practice/Demo1.java
+++ src/practice/Demo1.java
@@ -5,5 +5,6 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("GitJenkinsPractice_Project");
 		System.out.println("Adding this line to check GitHub PollSCM");
+		System.out.println("Adding this line in slave1 branch");
 	}
 }
